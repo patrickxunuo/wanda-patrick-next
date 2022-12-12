fddcb3cba5f80380cf0e765e89803f82696567ad
Patrick Xu <82446720+patrickxunuo@users.noreply.github.com>
2022-12-11 22:39:36 -0800

    change file naming

--- components/PageLoad.tsx
+++ components/PageLoad.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../styles/pageLoad.module.scss";
+import styles from "../styles/PageLoad.module.scss";
 import Image from "next/image";
 import { motion } from "framer-motion";
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import BackToTopBtn from "../components/BackToTopBtn";
-import styles from "../styles/home.module.scss";
+import styles from "../styles/Home.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
