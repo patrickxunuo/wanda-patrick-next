9b3a7aa65418c78bc612e97ef37477f827750b76
patrickxunuo <patrickxunuo@hotmail.com>
2023-05-07 00:09:20 -0700

    backToTop button scroll progress animation

--- components/BackToTopBtn.tsx
+++ components/BackToTopBtn.tsx
@@ -1,10 +1,11 @@
-import { motion } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
 import React from "react";
 import styles from "../styles/BackToTopBtn.module.scss";
 import { defaultVariantProps, homeDelayVariants } from "../utils/constant";
 
 const BackToTopBtn = () => {
   const goTop = () => window.scrollTo(0, 0);
+  const { scrollYProgress } = useScroll();
 
   return (
     <motion.div
@@ -13,6 +14,10 @@ const BackToTopBtn = () => {
       variants={homeDelayVariants}
       {...defaultVariantProps}
     >
+      <motion.div
+        className={styles.backdrop}
+        style={{ scaleY: scrollYProgress }}
+      />
       <div>Back To</div>
       <div>Top</div>
     </motion.div>
